048c7fcf09bb928605f1b86eeb291bcfd233ff69
officialshxdy <vandenbusscheshady@gmail.com>
2025-02-25 11:30:40 +0100

    Initial Commit

---
+++ boeksearch/boeksearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  boeksearchApp.swift
+//  boeksearch
+//
+//  Created by Shady Vandenbussche on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct boeksearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
